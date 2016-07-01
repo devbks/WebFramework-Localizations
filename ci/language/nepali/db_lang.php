253b9ad6d85ef7b7a7cd1f757d12d77edce5f8ce
devbks <merobato@gmail.com>
2016-07-01 17:18:13 +0545

    ci db_lang updated to nepali

--- ci/language/nepali/db_lang.php
+++ ci/language/nepali/db_lang.php
@@ -1,29 +1,29 @@
 <?php
 
-$lang['db_invalid_connection_str'] = 'Unable to determine the database settings based on the connection string you submitted.';
-$lang['db_unable_to_connect'] = 'Unable to connect to your database server using the provided settings.';
-$lang['db_unable_to_select'] = 'Unable to select the specified database: %s';
-$lang['db_unable_to_create'] = 'Unable to create the specified database: %s';
-$lang['db_invalid_query'] = 'The query you submitted is not valid.';
-$lang['db_must_set_table'] = 'You must set the database table to be used with your query.';
-$lang['db_must_use_set'] = 'You must use the "set" method to update an entry.';
-$lang['db_must_use_index'] = 'You must specify an index to match on for batch updates.';
-$lang['db_batch_missing_index'] = 'One or more rows submitted for batch updating is missing the specified index.';
-$lang['db_must_use_where'] = 'Updates are not allowed unless they contain a "where" clause.';
-$lang['db_del_must_use_where'] = 'Deletes are not allowed unless they contain a "where" or "like" clause.';
-$lang['db_field_param_missing'] = 'To fetch fields requires the name of the table as a parameter.';
-$lang['db_unsupported_function'] = 'This feature is not available for the database you are using.';
-$lang['db_transaction_failure'] = 'Transaction failure: Rollback performed.';
-$lang['db_unable_to_drop'] = 'Unable to drop the specified database.';
-$lang['db_unsuported_feature'] = 'Unsupported feature of the database platform you are using.';
-$lang['db_unsuported_compression'] = 'The file compression format you chose is not supported by your server.';
-$lang['db_filepath_error'] = 'Unable to write data to the file path you have submitted.';
-$lang['db_invalid_cache_path'] = 'The cache path you submitted is not valid or writable.';
-$lang['db_table_name_required'] = 'A table name is required for that operation.';
-$lang['db_column_name_required'] = 'A column name is required for that operation.';
-$lang['db_column_definition_required'] = 'A column definition is required for that operation.';
-$lang['db_unable_to_set_charset'] = 'Unable to set client connection character set: %s';
-$lang['db_error_heading'] = 'A Database Error Occurred';
+$lang['db_invalid_connection_str'] = 'तपाईंले दिनु भएको कनेक्सन शब्दको[string] अनुरुप डेटाबेससँग सम्बन्ध निर्धारण गर्न सकिएन ।';
+$lang['db_unable_to_connect'] = 'तपाईंले दिनु भएको सेतीङ अनुसार डेटाबेससँग सम्बन्ध बनाउन सकिएन ।';
+$lang['db_unable_to_select'] = 'निर्देशित गरिएको डेटाबेसलाई चयन गर्न असमर्थ: %s';
+$lang['db_unable_to_create'] = 'निर्देशित गरिएको डेटाबेसलाई निर्माण गर्न असमर्थ: %s';
+$lang['db_invalid_query'] = 'पेशगरिएको प्रश्न मान्य छैन। ';
+$lang['db_must_set_table'] = 'तपाईं आफ्नो प्रश्न प्रयोग गर्न डाटाबेस तालिका सेट गर्नु पर्छ।';
+$lang['db_must_use_set'] = 'तपाईं प्रविष्टि[entry] अपडेट गर्न "सेट" विधि प्रयोग गर्नुपर्छ।.';
+$lang['db_must_use_index'] = 'तपाईं ब्याच अपडेटका लागि मेल खाने एक सूचकांक [index] निर्दिष्ट गर्नुपर्छ।';
+$lang['db_batch_missing_index'] = 'ब्याच अद्यावधिकका लागि पेस गरिएको एक अथवा एक भन्दा धेरै पङ्क्तिहरूबाट निर्दिष्ट गरिएको सूचकांक हराइरहेको छ।';
+$lang['db_must_use_where'] = '"where" खण्ड समावेश नभएसम्म तेस्लाई अपडेट अनुमति छैन।';
+$lang['db_del_must_use_where'] = 'तेस्मा "where" वा "like" खण्ड समावेश नभएसम्म मेटाउने अनुमति छैन।';
+$lang['db_field_param_missing'] = 'चाहिएको क्षेत्रहरू ल्याउनका लागि तालिकाको नाम प्यारामितर जसरी पठाउनु पर्ने हुन्छ। ';
+$lang['db_unsupported_function'] = 'यो सुविधा तपाईँले प्रयोग गरिरहनु भएको डेटाबेस लागि उपलब्ध छैन।';
+$lang['db_transaction_failure'] = 'कारोबार बिफल भयो : रोलब्याक गरियो।';
+$lang['db_unable_to_drop'] = 'निर्दिष्ट डेटाबेस फाल्न गर्न असमर्थ भयो।';
+$lang['db_unsuported_feature'] = 'तपाईंले प्रयोग गर्नु भएको डेटाबेस मन्चको[platform] समर्थन नगरने सुबिधा।';
+$lang['db_unsuported_compression'] = 'तपाईं छनौट फाइल सङ्कुचन ढाँचा तपाईंको सर्भर द्वारा समर्थित छैन।';
+$lang['db_filepath_error'] = 'पाईं पेश गरेको फाइल मार्ग डेटा लेख्न असमर्थ।';
+$lang['db_invalid_cache_path'] = 'तपाईं पेश क्यास[cache] बाटो मान्य वा लेख्न योग्य छैन।';
+$lang['db_table_name_required'] = 'एक तालिका नाम भनेर कार्यका लागि आवश्यक छ।';
+$lang['db_column_name_required'] = 'एक स्तम्भ [column] नाम भनेर कार्यका लागि आवश्यक छ।';
+$lang['db_column_definition_required'] = 'एक स्तम्भ[column] परिभाषा  कार्यका लागि आवश्यक छ।';
+$lang['db_unable_to_set_charset'] = 'ग्राहक जडान वर्ण सेट गर्न असमर्थ: %s';
+$lang['db_error_heading'] = 'डाटाबेसमा त्रुटि उत्पन्न भयो ';
 
 /* End of file db_lang.php */
 /* Location: ./system/language/english/db_lang.php */
