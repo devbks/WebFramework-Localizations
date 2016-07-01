38570d13ef576e4eb8af04fa2175e57942255b92
devbks <merobato@gmail.com>
2016-07-01 16:45:07 +0545

    Calendar_lang.php updated

---
+++ language/nepali/calendar_lang.php
@@ -0,0 +1,51 @@
+<?php
+
+$lang['cal_su']			= "आइत";
+$lang['cal_mo']			= "सोम";
+$lang['cal_tu']			= "मंगल";
+$lang['cal_we']			= "बुध";
+$lang['cal_th']			= "बिही";
+$lang['cal_fr']			= "शुक्";
+$lang['cal_sa']			= "शनि";
+$lang['cal_sun']		= "आइत";
+$lang['cal_mon']		= "सोम";
+$lang['cal_tue']		= "मंगल";
+$lang['cal_wed']		= "बुध";
+$lang['cal_the']		= "बिही";
+$lang['cal_fri']		= "शुक्";
+$lang['cal_sat']		= "शनि";
+$lang['cal_sunday']		= "आइतबार";
+$lang['cal_monday']		= "सोमबार";
+$lang['cal_tuesday']		= "मंगलबार";
+$lang['cal_wednesday']		= "बुधबार";
+$lang['cal_thursday']		= "बिहिबार";
+$lang['cal_friday']		= "शुक्रबार";
+$lang['cal_saturday']		= "शनिबार";
+$lang['cal_jan']		= "Jan";
+$lang['cal_feb']		= "Feb";
+$lang['cal_mar']		= "Mar";
+$lang['cal_apr']		= "Apr";
+$lang['cal_may']		= "May";
+$lang['cal_jun']		= "Jun";
+$lang['cal_jul']		= "Jul";
+$lang['cal_aug']		= "Aug";
+$lang['cal_sep']		= "Sep";
+$lang['cal_oct']		= "Oct";
+$lang['cal_nov']		= "Nov";
+$lang['cal_dec']		= "Dec";
+$lang['cal_january']		= "January";
+$lang['cal_february']		= "February";
+$lang['cal_march']		= "March";
+$lang['cal_april']		= "April";
+$lang['cal_mayl']		= "May";
+$lang['cal_june']		= "June";
+$lang['cal_july']		= "July";
+$lang['cal_august']		= "August";
+$lang['cal_september']	= "September";
+$lang['cal_october']	= "October";
+$lang['cal_november']	= "November";
+$lang['cal_december']	= "December";
+
+
+/* End of file calendar_lang.php */
+/* Location: ./system/language/english/calendar_lang.php */
