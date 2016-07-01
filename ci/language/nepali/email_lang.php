5438e17030466646bde6761f47dcc20248183ce6
devbks <merobato@gmail.com>
2016-07-01 17:33:33 +0545

    ci email_lang.php updated

--- ci/language/nepali/email_lang.php
+++ ci/language/nepali/email_lang.php
@@ -1,23 +1,23 @@
 <?php
 
-$lang['email_must_be_array'] = "The email validation method must be passed an array.";
-$lang['email_invalid_address'] = "Invalid email address: %s";
-$lang['email_attachment_missing'] = "Unable to locate the following email attachment: %s";
-$lang['email_attachment_unreadable'] = "Unable to open this attachment: %s";
-$lang['email_no_recipients'] = "You must include recipients: To, Cc, or Bcc";
-$lang['email_send_failure_phpmail'] = "Unable to send email using PHP mail().  Your server might not be configured to send mail using this method.";
-$lang['email_send_failure_sendmail'] = "Unable to send email using PHP Sendmail.  Your server might not be configured to send mail using this method.";
-$lang['email_send_failure_smtp'] = "Unable to send email using PHP SMTP.  Your server might not be configured to send mail using this method.";
-$lang['email_sent'] = "Your message has been successfully sent using the following protocol: %s";
-$lang['email_no_socket'] = "Unable to open a socket to Sendmail. Please check settings.";
-$lang['email_no_hostname'] = "You did not specify a SMTP hostname.";
-$lang['email_smtp_error'] = "The following SMTP error was encountered: %s";
-$lang['email_no_smtp_unpw'] = "Error: You must assign a SMTP username and password.";
-$lang['email_failed_smtp_login'] = "Failed to send AUTH LOGIN command. Error: %s";
-$lang['email_smtp_auth_un'] = "Failed to authenticate username. Error: %s";
-$lang['email_smtp_auth_pw'] = "Failed to authenticate password. Error: %s";
-$lang['email_smtp_data_failure'] = "Unable to send data: %s";
-$lang['email_exit_status'] = "Exit status code: %s";
+$lang['email_must_be_array'] = "इमेल मान्यता विधि श्रेणीको[array] पारित हुनुपर्छ।";
+$lang['email_invalid_address'] = "अवैध इमेल ठेगाना: %s";
+$lang['email_attachment_missing'] = "निम्न इमेल संलग्न पत्ता लगाउन असमर्थ: %s";
+$lang['email_attachment_unreadable'] = "यो संलग्न खोल्न असमर्थ: %s";
+$lang['email_no_recipients'] = "To, Cc, वा Bcc: तपाईं प्राप्तकर्ताहरूसँग समावेश गर्नुपर्छ";
+$lang['email_send_failure_phpmail'] = "PHP mail() प्रयोगगरि इमेल पठाउन असमर्थ।  तपाईंको सर्भर यो प्रबिधी प्रयोग गरी मेल पठाउन असमर्थ हुन सक्छ। ";
+$lang['email_send_failure_sendmail'] = "PHP Sendmail प्रयोगगरि इमेल पठाउन असमर्थ।   तपाईंको सर्भर यो प्रबिधी प्रयोग गरी मेल पठाउन असमर्थ हुन सक्छ।";
+$lang['email_send_failure_smtp'] = "PHP SMTP प्रयोगगरि इमेल पठाउन असमर्थ।   तपाईंको सर्भर यो प्रबिधी प्रयोग गरी मेल पठाउन असमर्थ हुन सक्छ। ";
+$lang['email_sent'] = "तपाईंको सन्देश सफलतापूर्वक निम्न प्रोटोकल प्रयोग पठाइएको छ: %s";
+$lang['email_no_socket'] = "Sendmail गर्न सकेट खोल्न असमर्थ । सेटिङ जाँच गर्नुहोस्।";
+$lang['email_no_hostname'] = "तपाईं एउटा SMTP होस्टनाम निर्दिष्ट गर्नुभएन।";
+$lang['email_smtp_error'] = "निम्न SMTP त्रुटि सामना गर्न पर्यो: %s";
+$lang['email_no_smtp_unpw'] = "त्रुटि: तपाईं SMTP प्रयोगकर्ता नाम र पासवर्ड प्रदान गर्नु पर्छ।";
+$lang['email_failed_smtp_login'] = "AUTH LOGIN आदेश पठाउन असफल भयो। त्रुटि:  %s";
+$lang['email_smtp_auth_un'] = "प्रयोगकर्ता नाम प्रमाणित गर्न असफल भयो। त्रुटि:  %s";
+$lang['email_smtp_auth_pw'] = "पासवर्ड प्रमाणित गर्न असफल त्रुटि:  %s";
+$lang['email_smtp_data_failure'] = "डाटा पठाउन असमर्थ:  %s";
+$lang['email_exit_status'] = "स्थिति निकास कोड: %s";
 
 
 /* End of file email_lang.php */
